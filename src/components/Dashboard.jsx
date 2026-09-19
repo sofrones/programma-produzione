5e9cfac77cafd2c78781c3c13521ddb84c2d8cd2
Luca <f.sofrones@gmail.com>
2026-09-19 03:09:48 +0200

    Sostituisce l'icona di stato modificabile con un lucchetto aperto verde

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -500,7 +500,7 @@ export default function SupplierDashboard() {
                       <span title={editable ? 'Modificabile' : 'Bloccato'}>
                         {editable ? (
                           <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-green-600 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H7a2 2 0 00-2 2v6a2 2 0 002 2zm0-10V7a4 4 0 017.75-1.5" />
                           </svg>
                         ) : (
                           <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-gray-400 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
